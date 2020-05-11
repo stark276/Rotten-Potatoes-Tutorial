6c99e09be69980e2654380fc29f5cdaa2ac8214d
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2020-05-11 16:19:19 -0700

    Added MongoDB and Review model

--- app.js
+++ app.js
@@ -6,16 +6,30 @@ const express = require('express')
 const app = express()
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+//mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
+const Review = mongoose.model('Review', {
+  title: String,
+  movieTitle: String
+});
 
 // OUR MOCK ARRAY OF PROJECTS
-let reviews = [
-  { title: "Great Review", movieTitle: "Batman II" },
-  { title: "Awesome Movie", movieTitle: "Titanic" }
-]
+// let reviews = [
+//   { title: "Great Review", movieTitle: "Batman II" },
+//   { title: "Awesome Movie", movieTitle: "Titanic" },
+//   { title: "woow", movieTitle: "Inception" }
+// ]
 
 // INDEX
 app.get('/', (req, res) => {
-  res.render('reviews-index', { reviews: reviews });
+  Review.find()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
 
 app.listen(3000, () => {
