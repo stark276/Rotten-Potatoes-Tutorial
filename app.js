dcc3a49a3b933e21572729823f380fe22a04e227
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2020-05-11 18:12:24 -0700

    Added MongoDB and Review model

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
 // app.js
 var exphbs = require('express-handlebars');
-
-
+const mongoose = require('mongoose');
 const express = require('express')
+
 const app = express()
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-//mongoose
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
+
+// DB
+mongoose.connect('mongodb://localhost/rotten-potatoes', {
+  useNewUrlParser: true
+});
+
 const Review = mongoose.model('Review', {
   title: String,
   movieTitle: String
 });
 
-// OUR MOCK ARRAY OF PROJECTS
-// let reviews = [
-//   { title: "Great Review", movieTitle: "Batman II" },
-//   { title: "Awesome Movie", movieTitle: "Titanic" },
-//   { title: "woow", movieTitle: "Inception" }
-// ]
-
 // INDEX
 app.get('/', (req, res) => {
-  Review.find()
-    .then(reviews => {
-      res.render('reviews-index', { reviews: reviews });
+  Review.find()  // returns promise 
+    .then(reviews => { // resolve it 
+      res.render('reviews-index', {
+        reviews: reviews
+      });
     })
-    .catch(err => {
+    .catch(err => { // reject
       console.log(err);
     })
 })
@@ -35,3 +33,5 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
+
+
