4566f182b54fc0d43ec9ab8e67fcff818005a98b
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2020-05-14 02:23:38 -0700

    Users can destroy reviews

--- app.js
+++ app.js
@@ -96,6 +96,15 @@ app.put('/reviews/:id', (req, res) => {
       console.log(err.message)
     })
 })
+// DELETE
+app.delete('/reviews/:id', function (req, res) {
+  console.log("DELETE review")
+  Review.findByIdAndRemove(req.params.id).then((review) => {
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
